5d970b11a4fe212d9dc5d0ee2bcf33b871099a6b
Danila Rusak <skinny4550@gmail.com>
2023-06-21 14:43:06 +0200

    UI

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantThingy.Pages;
+
+public class About : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Pages/Delivery.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantThingy.Pages;
+
+public class Delivery : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
