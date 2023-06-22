b0e9739609e11aabb2ab9f1b30787f75277d648c
72152AG <129647623+72152AG@users.noreply.github.com>
2023-06-22 21:24:30 +0200

    v2 for final
    github? more than 100 when?

---
+++ Migrations/20230622175005_UpdateOrderTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantThingy.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Orders",
+                newName: "Street");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "Street",
+                table: "Orders",
+                newName: "Address");
+        }
+    }
+}
