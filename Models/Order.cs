f32486bea026a2ab4306a390567639996bdfb429
Danila Rusak <skinny4550@gmail.com>
2023-06-22 03:21:01 +0200

    Button fixed

--- Models/Order.cs
+++ Models/Order.cs
@@ -4,14 +4,10 @@ public class Order
 {
     public int OrderId { get; set; }
     public string Name { get; set; }
-    
     public string Street { get; set; }
-    
     public string City { get; set; }
-    
     public string Phone { get; set; }
-
-
+    
     // Collection navigation property for order items
     public List<OrderItem> Items { get; set; }
 }
